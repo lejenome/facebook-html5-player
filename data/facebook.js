48b4ebcb64ad79d445d261e1b557dc491ff55824
Moez Bouhlel <bmoez.j@gmail.com>
2014-12-05 12:47:22 +0000

    fix width and heigth of the player

--- data/facebook.js
+++ data/facebook.js
@@ -1,5 +1,6 @@
-(function(){
-	"use strict";
+(function() {
+    "use strict";
+
     function createNode(type, obj, data, style) {
         var node = document.createElement(type);
         if (obj)
@@ -17,19 +18,20 @@
         return node;
     }
 
-	var params = document.body.innerHTML.match(/\"params\",(\"[^\"]*\")/)[1];
-	params =  JSON.parse(decodeURIComponent(JSON.parse(params)));
-	var url  = params.video_data[0].sd_src || params.video_data[0].hd_src;
-	var player = createNode("video", {
-		width: "100%",
-		heigth: "100%",
-//		preload: true,
-		controls: true,
-		autoplay: true,
-		src: url
-	});
+    var params = document.body.innerHTML.match(/\"params\",(\"[^\"]*\")/)[1];
+    params = JSON.parse(decodeURIComponent(JSON.parse(params)));
+    var url = params.video_data[0].sd_src || params.video_data[0].hd_src;
+    var player = createNode("video", {
+        //		preload: true,
+        controls: true,
+        autoplay: true,
+        src: url
+    }, {}, {
+        width: "100%",
+        heigth: "100%"
+    });
 
-	document.getElementsByClassName("_53j5")[0].innerHTML = "";
-	document.getElementsByClassName("_53j5")[0].appendChild(player);
-//        location = url;
+    document.getElementsByClassName("_53j5")[0].innerHTML = "";
+    document.getElementsByClassName("_53j5")[0].appendChild(player);
+    //        location = url;
 }());
